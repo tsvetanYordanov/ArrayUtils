581d17eec4213a191139f19b7457c79892e643c7
Tsvetan Yordanov <tsvetyordanov@gmail.com>
2017-05-06 14:24:44 +0100

    #1 Implemented project structure

---
+++ array_splitter/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Performs utility function of splitting arrays"""
+
+from array_splitter import metadata
+
+
+__version__ = metadata.version
+__author__ = metadata.authors[0]
+__license__ = metadata.license
+__copyright__ = metadata.copyright
---
+++ array_splitter/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'array_splitter'
+project = "ArraySplitter"
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = 'Performs utility function of splitting arrays'
+authors = ['Tsvetan Yordanov']
+authors_string = ', '.join(authors)
+emails = ['tsvetyordanov@gmail.com']
+license = 'GNU'
+copyright = '2017 ' + authors_string
+url = 'https://github.com/tsvetanYordanov/ArrayUtils'
