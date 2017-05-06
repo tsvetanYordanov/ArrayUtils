71a9a03157aadadae8756d6b8dbc103995f8ba68
Tsvetan Yordanov <tsvetyordanov@gmail.com>
2017-05-06 16:12:13 +0100

    #3 add additional testing. implement array split functionality

--- array_splitter/array_splitter_util.py
+++ array_splitter/array_splitter_util.py
@@ -1,5 +1,4 @@
 class ArraySplitterUtil:
-
     def __init__(self, array):
         self.array = array
 
@@ -7,5 +6,25 @@ class ArraySplitterUtil:
         return "Array splitter: " % self.array
 
     def split_array(self, num_of_splits):
-        # TODO: implement me!
-        return self.array
+        arr = self.array
+        array_len = len(arr)
+        arr_remainder = (array_len % num_of_splits)
+        arr_quotient = array_len / num_of_splits
+        result = []
+        remainder_arr = []
+        print "num of splits required: " + str(num_of_splits)
+        print "input is: " + str(arr)
+        if arr_remainder > 0:
+            remainder_arr = arr[-arr_remainder:]
+            num_of_splits -= 1
+            array_len -= arr_remainder
+            arr_quotient = array_len / num_of_splits
+
+        for i in range(num_of_splits):
+            curr_split = arr[i * arr_quotient:i * arr_quotient + arr_quotient]
+            print "curr split is: " + str(curr_split)
+            result.append(curr_split)
+        if arr_remainder > 0:
+            result.append(remainder_arr)
+        print "resulting split is: " + str(result)
+        return result
--- tests/test_array_splitter_util.py
+++ tests/test_array_splitter_util.py
@@ -13,29 +13,33 @@ from array_splitter.array_splitter_util import ArraySplitterUtil
 
 class TestArraySplitterUtil(object):
     @parametrize('array',
-                 [[1, 2, 3], [1, 2, 3, 4, 5, 6],
+                 [[1, 2, 3, 12, 42, 56, 99], [1, 2, 3, 4, 5, 6],
                   ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'klmn'],
-                  [(1, 23), (45, 6), (7, 89), (0, 0, -1)]])
-    @parametrize('num_of_splits', [1])
+                  [(1, 23), (45, 6), (7, 89), (0, 0, -1),
+                   (1, 1), (7, 8, 9), (0, 0, 0)]])
+    @parametrize('num_of_splits', [1, 2, 3, 4, 5])
     def test_split_array(self, array, num_of_splits):
         array_splitter_util = ArraySplitterUtil(array)
         result = array_splitter_util.split_array(num_of_splits)
         array_len = len(array)
         arr_remainder = (array_len % num_of_splits)
         arr_quotient = array_len / num_of_splits
-        expected_num_of_splits = arr_quotient + arr_remainder
+        expected_split_len = arr_quotient
+        if arr_remainder > 0:
+            expected_split_len = (array_len - arr_remainder)
+            expected_split_len /= (num_of_splits - 1)
 
-        # Should be split into arrays num_of_splits arrays
-        assert expected_num_of_splits == len(result)
+        # Should be split into num_of_splits arrays
+        assert num_of_splits == len(result)
         # They should be equally sized arrays if original array is divisible
         # by num_of_splits, otherwise all but the last array should be
         # equally sized
         if arr_remainder == 0:
             for arr in result:
-                assert arr_quotient == len(arr)
+                assert expected_split_len == len(arr)
         else:
             for arr in result[:-1]:
-                assert arr_quotient == len(arr)
+                assert expected_split_len == len(arr)
         # If the size of the original array cannot be divided equally
         # the final part should have length equal to the remainder
         if arr_remainder != 0:
