97df8b345827423441d0f6a49e97e11970e081cf
Tsvetan Yordanov <tsvetyordanov@gmail.com>
2017-05-06 16:30:37 +0100

    Update array_splitter_util.py
    removed unnecesarry prints + added comments

--- array_splitter/array_splitter_util.py
+++ array_splitter/array_splitter_util.py
@@ -12,19 +12,21 @@ class ArraySplitterUtil:
         arr_quotient = array_len / num_of_splits
         result = []
         remainder_arr = []
-        print "num of splits required: " + str(num_of_splits)
-        print "input is: " + str(arr)
         if arr_remainder > 0:
+            # If there is a remainder in the division
+            # then store the last remainder number of elements
+            # in remainder_arr, decrease the number of splits
+            # and recalculate the quotient
             remainder_arr = arr[-arr_remainder:]
             num_of_splits -= 1
             array_len -= arr_remainder
             arr_quotient = array_len / num_of_splits
-
+        # split original array to equal-sized arrays
+        # where each will be of size equal to the quotient
         for i in range(num_of_splits):
             curr_split = arr[i * arr_quotient:i * arr_quotient + arr_quotient]
-            print "curr split is: " + str(curr_split)
             result.append(curr_split)
+        # add the remainder_arr as the last if there is one
         if arr_remainder > 0:
             result.append(remainder_arr)
-        print "resulting split is: " + str(result)
         return result
